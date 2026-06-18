04058cecbd9a25c23836dccee012902b747b8092
zstata <33490398+zstata@users.noreply.github.com>
2026-06-17 21:37:49 -0600

    added compatibility checker

--- pc_draftclass_converter.py
+++ pc_draftclass_converter.py
@@ -56,16 +56,36 @@ HEAD_COLUMNS = [
 
 DONOR_COLUMNS = CAP_COLUMNS + HEAD_COLUMNS
 
+def check_compatability(pc, donor):
+    if len(pc) != len(donor):
+        raise ValueError(
+            f"Row count mismatch: PC={len(pc)} donor={len(donor)}"
+        )
+    
+    if len(pc.columns) != len(donor.columns):
+        raise ValueError(
+            f"Column count mismatch: PC={len(pc.columns)} donor={len(donor.columns)}"
+        )
+    
+    for col in pc.columns:
+        if col not in donor.columns:
+            raise ValueError(
+                f"PS3 CSV does not have {col} column!"
+            )
+        
+    for col in donor.columns:
+        if col not in pc.columns:
+            raise ValueError(
+                f"PC CSV does not have {col} column!"
+            )
+
 
 def convert(pc_csv, donor_csv, output_csv):
 
     pc = pd.read_csv(pc_csv, encoding="utf-16le")
     donor = pd.read_csv(donor_csv, encoding="utf-16le")
 
-    if len(pc) != len(donor):
-        raise ValueError(
-            f"Row count mismatch: PC={len(pc)} donor={len(donor)}"
-        )
+    check_compatability(pc, donor)
 
     # Force PS3 values
     for col, value in FIXED_VALUES.items():
@@ -85,7 +105,7 @@ def convert(pc_csv, donor_csv, output_csv):
 
         pc[col] = donor[col].values
 
-    pc.to_csv(output_csv, index=False)
+    pc.to_csv(output_csv, index=False, encoding="utf-16le")
 
     print(f"Saved {output_csv}")
 
